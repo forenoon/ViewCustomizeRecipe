535f05fb5237541319028cf9480e807e4af29bfe
forenoon <forenoon@mac.com>
2017-10-31 14:30:03 +0900

    初回アップロード

---
+++ HistoricalAtom/JavaScript.js
@@ -0,0 +1,14 @@
+/* ------ view-customizes:
+ マイページへAtom(History)を追加
+----------------------- */
+$(function() {
+    var span = $("div#content .other-formats a.csv").parent().clone(true);
+    span.find("a.csv").remove();
+    
+    var aAtom = $("div#content .other-formats a.atom").clone();
+    aAtom.text("Atom(History)");
+    aAtom.attr("href", aAtom.attr("href").replace("/issues.atom", "/issues/changes.atom"));
+    span.append(aAtom);
+    
+    $("div#content .other-formats a.atom").parent().after(span);
+})
